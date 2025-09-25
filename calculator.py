6aa4801ab5a6749f3a443b732dd27afc5fbadd03
takep <74190436+takep6@users.noreply.github.com>
2025-09-25 15:27:21 +0900

    各関数のドキュメントの詳細を書いた

--- calculator.py
+++ calculator.py
@@ -1,21 +1,49 @@
 
-def add(a, b):
-    """This function adds two numbers"""
+def add(a: float, b: float) -> float:
+    """Adds two floating point numbers and returns the result.
+
+    Args:
+        a: The first floating point number.
+        b: The second floating point number.
+    Returns:
+        The sum of the two floating point numbers.
+    """
     return a + b
 
 
-def subtract(a, b):
-    """This function subtracts two numbers"""
+def subtract(a: float, b: float) -> float:
+    """Subtracts two floating point numbers and returns the result.
+
+    Args:
+        a: The first floating point number.
+        b: The second floating point number.
+    Returns:
+        The difference of the two floating point numbers.
+    """
     return a - b
 
 
-def multiply(a, b):
-    """This function multiplies two numbers"""
+def multiply(a: float, b: float) -> float:
+    """Multiplies two floating point numbers and returns the result.
+
+    Args:
+        a: The first floating point number.
+        b: The second floating point number.
+    Returns:
+        The product of the two floating point numbers.
+    """
     return a * b
 
 
-def divide(a, b):
-    """This function divides two numbers"""
+def divide(a: float, b: float) -> float:
+    """Divides two floating point numbers and returns the result. Raises a ValueError if b is zero.
+
+    Args:
+        a: The first floating point number.
+        b: The second floating point number.
+    Returns:
+        The quotient of the two floating point numbers.
+    """
     if b == 0:
         raise ValueError("Cannot divide by zero!")
     return a / b
